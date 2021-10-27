45b7a30e8c8f79548f24b5334881e45846560df1
karyun.cheung <karyun.cheung@gmail.com>
2021-10-27 11:39:21 +0100

    toa

--- toa.go
+++ toa.go
@@ -6,7 +6,18 @@ func ToA(s string) string {
 	var result string
 	words := strings.Split(s, " ")
 	for a := 0; a < len(words)-1; a++ {
-		if words[a] == "a" {
+		if a == 0 {
+			result += words[a]
+		} else if words[a] != "A" && words[a] != "a" {
+			result += " " + words[a]
+		} else if words[a] == "A" {
+			if strings.HasPrefix(words[a+1], "a") || strings.HasPrefix(words[a+1], "e") || strings.HasPrefix(words[a+1], "i") || strings.HasPrefix(words[a+1], "o") || strings.HasPrefix(words[a+1], "u") || strings.HasPrefix(words[a+1], "h") {
+				result += " " + "An"
+			}
+		} else if words[a] == "a" {
+			if strings.HasPrefix(words[a+1], "a") || strings.HasPrefix(words[a+1], "e") || strings.HasPrefix(words[a+1], "i") || strings.HasPrefix(words[a+1], "o") || strings.HasPrefix(words[a+1], "u") || strings.HasPrefix(words[a+1], "h") {
+				result += " " + "an"
+			}
 		}
 	}
 	return result
